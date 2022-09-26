314217c383cb32c943bb896ef4e3a11995f4edc3
Alan Veiga <alan.veiga@hotmail.com>
2022-09-26 16:17:11 -0300

    first commit

---
+++ backend/database/db.js
@@ -0,0 +1,8 @@
+//module.exports = {
+//  db: 'mongodb://localhost:27017/angular8mean'
+//};
+
+
+module.exports = {
+  db: 'mongodb+srv://dbadmin:senhasegura@tpprojectcluster0.zbonpra.mongodb.net/tpdbteste?retryWrites=true&w=majority'
+};
